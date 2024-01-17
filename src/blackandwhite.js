89c510838d44a828e3065b815cf567e55e2c70dd
internetperu <internetperu@dyn.internet.com.pe>
2024-01-16 20:36:58 -0500

    Update 2

--- src/blackandwhite.js
+++ src/blackandwhite.js
@@ -1,40 +1,22 @@
-/* src/blackandwhite.js */
-/*  */
+/**
+ * blackandwhite - Easy package.json exports.
+ * 
+ * Author: Jack Rojas +51930222760 [jack @ internet.com.pe
+ * Website: https://internet.pe/
+ * Licensed under the MIT License.
+ */
 
-//var firstParagraph = document.querySelector("p")
-//firstParagraph.classList.add("baw_ip9_e99785r")
 function BlackAndWhite(options){
-    
     let _this = this;
     let defaultOptions = {
         container: ".blackandwhite"
     }
     options = {...defaultOptions, ...options};
-    
     this.init = function() {
-        
-        /*var elements_bw = document.querySelector(options.container);
-		elements_bw.classList.add("baw_ip9_e99785r"); */
-		
 		document.querySelectorAll(options.container).forEach((element_app, index) => {
-            // console.log(slide);
-            console.log("Poniendo clase al elemento "+options.container+" indice: "+index);
+            //console.log("Poniendo clase al elemento "+options.container+" indice: "+index);
             element_app.classList.add("baw_ip9_e99785r");
-            /*slides[index] = slide;
-            slides[index].style = "display:none";
-            slides[index].className += " too-slide-single-slide too-slide-fade";
-            */
         });
-        //document.querySelector(options.container).className += " too-slide-slider-container";
-        /* document.querySelectorAll(options.slidesClass).forEach((slide, index) => {
-            // console.log(slide);
-            slides[index] = slide;
-            slides[index].style = "display:none";
-            slides[index].className += " too-slide-single-slide too-slide-fade";
-        }); */
-        
     }
-
     this.init();
-    
 }
